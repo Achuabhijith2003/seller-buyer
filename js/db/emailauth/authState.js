bf410721f6b5c92c1d98e5f6ed64d52fd5650ee2
JR <Achuabhijithjr@outlook.com>
2024-06-28 23:36:19 +0530

    remove side drawer and rearrage the laout in desktop and mobile view

--- js/db/emailauth/authState.js
+++ js/db/emailauth/authState.js
@@ -1,41 +1,35 @@
 import { auth } from '../firebaseinit.js';
-import { onAuthStateChanged ,signOut} from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
+import { onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
 
 // Get elements
-const signoutButton = document.getElementById('sign-out-button');
 const loginButton = document.getElementById('login-button');
 const profileIcon = document.getElementById('profile-icon');
+const cartButton = document.getElementById('cart-button');
+const signOutButton = document.getElementById('sign-out-button');
 
 onAuthStateChanged(auth, (user) => {
     if (user) {
-        // User is signed in
         loginButton.style.display = 'none';
         profileIcon.style.display = 'block';
         profileIcon.addEventListener('click', () => {
             window.location.href = 'profile.html';
         });
+        signOutButton.style.display = 'block';
     } else {
-        // User is signed out
         loginButton.style.display = 'block';
         profileIcon.style.display = 'none';
         loginButton.addEventListener('click', () => {
             window.location.href = 'login.html';
         });
+        signOutButton.style.display = 'none';
     }
 });
 
-signoutButton.addEventListener('click', () => {
-    signOut(auth)
-        .then(() => {
-            console.log('Logged out');
-            loginButton.style.display = 'block';
-        profileIcon.style.display = 'none';
-        signoutButton.style.display='none';
-        loginButton.addEventListener('click', () => {
-            window.location.href = 'login.html';
-        });
-        })
-        .catch((error) => {
-            console.error('Logout Error:', error);
-        });
+signOutButton.addEventListener('click', () => {
+    signOut(auth).then(() => {
+        console.log('Logged out');
+        location.reload();
+    }).catch((error) => {
+        console.error('Logout Error:', error);
+    });
 });
